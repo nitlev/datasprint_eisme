982f88ad3784438f9371c87a3cef9304be7c97fb
Veltin Dupont <vdupont@octo.com>
2016-12-04 12:01:17 +0100

    starting to add sse

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import json
 
 from flask import Flask
+from flask import render_template
 from flask import request
-from wit import Wit
+from flask_sse import sse
 
+from wit import Wit
 from wit_functions import send_message_to_client
 
 
@@ -15,18 +17,25 @@ def read_token():
 
 client = Wit(read_token())
 app = Flask(__name__)
+app.config["REDIS_URL"] = "redis://localhost"
+app.register_blueprint(sse, url_prefix='/stream')
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template("index.html")
+
+
+@app.route('/hello')
+def publish_hello():
+    sse.publish({"message": "Hello!"}, type='greeting')
+    return "Message sent!"
 
 
 @app.route('/message', methods=['POST'])
 def send_message():
     message = request.get_json().get("message", "")
     return json.dumps(send_message_to_client(client, message))
-
 
 if __name__ == '__main__':
     app.run()
--- tests/tests.py
+++ tests/tests.py
@@ -1,18 +1,7 @@
-from app import hello_world
 from wit_functions import MockWitClient, send_message_to_client
 
 
 class TestClass:
-    def test_hello_world_should_return_HelloWorld(self):
-        # Given
-        #
-
-        # When
-        resp = hello_world()
-
-        # Assert
-        assert resp == "Hello World!"
-
     def test_send_message_should_return_bot_message(self):
         # Given
         client = MockWitClient()
